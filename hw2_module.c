60bdd7110be14830eceeabf87064cdeb2d797c02
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-12-16 12:48:07 +0900

    feat: add process info

--- hw2_module.c
+++ hw2_module.c
@@ -2,15 +2,60 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/sched/signal.h>
+
+// DATA
+#define STUDENT_NAME "Shiwon Jeong"
+#define STUDENT_ID "2018199061"
+
+// Prototypes
+void print_bar(void);
 
 // Tasklet Handler
 void tasklet_func(unsigned long data);
 
+// Utils
+
+void print_bar()
+{
+    printk("************************************\n");
+}
+
+// Declare Tasklet
 DECLARE_TASKLET_OLD(my_tasklet, tasklet_func);
 
 void tasklet_func(unsigned long data)
 {
-	printk("my_tasklet run: do what the tasklet want to do...\n");
+	// get all tasks
+	struct task_struct *task_iter;
+    struct task_struct *target_task;
+	for_each_process(task_iter) {
+        if (task_iter->state == TASK_RUNNING) {
+            target_task = task_iter;
+            break;
+        }
+	}
+
+
+    int is_kernel_thread = 0;
+    if (target_task->mm == NULL) {
+        is_kernel_thread = 1;
+    }
+
+    
+
+    printk("Student ID: %s, Name: %s\n", STUDENT_ID, STUDENT_NAME);
+    printk("Virtual Memory Address Information\n");
+    printk("Process Name: %s\n", target_task->comm);
+    printk("Process id: %d\n", target_task->pid);
+
+    print_bar();
+
+    if (is_kernel_thread) {
+        printk("This is kernel thread!\n");
+        print_bar();
+        return;
+    }
 }
 
 static int __init hw2_init(void)
@@ -31,6 +76,6 @@ static void __exit hw2_exit(void)
 module_init(hw2_init);
 module_exit(hw2_exit);
 
-MODULE_AUTHOR("Sebastian Jeong");
+MODULE_AUTHOR("Shiwon Jeong");
 MODULE_DESCRIPTION("HW2 Module");
 MODULE_LICENSE("GPL");
--- runnerfile.sh
+++ runnerfile.sh
@@ -1,29 +1,40 @@
-task_comp() {
+task_compile() {
 	make
 }
 
-task_clean() {
+task_clean_compiled() {
 	make clean
 }
 
-task_insert() {
+task_insert_module() {
 	sudo insmod hw2_module.ko
 }
 
-task_list() {
+task_list_modules() {
 	lsmod | grep hw2
 }
 
-task_remove() {
+task_remove_module() {
 	sudo rmmod hw2_module.ko
 }
 
-task_log() {
+task_kernel_log() {
 	dmesg
 }
 
-task_all() {
-	comp
-	insert
-	log
+task_edit() {
+    vi ./hw2_module.c
+}
+
+
+task_cleanup() {
+	task_remove_module
+	task_clean_compiled
+}
+
+task_run() {
+	task_cleanup
+	task_compile
+	task_insert_module
+	task_kernel_log
 }
